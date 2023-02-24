96f541c33abe452876970502fb465e0beec73ad8
Heeho Ryou <heeho3464@gmail.com>
2023-02-24 15:20:15 -0800

    Move player setup Move enemies in progress

--- src/main/java/org/bcit/comp2522/project/Moveable.java
+++ src/main/java/org/bcit/comp2522/project/Moveable.java
@@ -2,11 +2,25 @@ package org.bcit.comp2522.project;
 
 public interface Moveable {
 
-  public Player player;
-  public ArrayList<Enemy> Enemies;
+   public Player player;
 
-  public void moveUP();
-  public void moveDown();
-  public void moveRight();
-  public void moveLeft();
+  public ArrayList<Waves> enemies;
+
+  public static void playerMove(char key) {
+    //keyboard keys to move player every direction (WASD)
+    if (key == 'w') {
+      yPos -= 5;
+    } else if (key == 'a') {
+      xPos -= 5;
+    } else if (key == 's') {
+      yPos += 5;
+    } else if (key == 'd') {
+      xPos += 5;
+    }
+  }
+
+  public static void trollMove(int i) {
+    i = Enemies.waveNumber;
+    for(int x = 0; x < )
+  }
 }
