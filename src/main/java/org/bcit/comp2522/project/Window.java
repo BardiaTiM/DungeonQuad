87ba83bc45d5932410fa3df8b8118f2c396a4d1d
Bardia <bilbeigitimouri@my.bcit.ca>
2023-02-24 13:03:08 -0800

    Created Window.java, added basic methods. Needs work.

---
+++ src/main/java/org/bcit/comp2522/project/Window.java
@@ -0,0 +1,23 @@
+package org.bcit.comp2522.project;
+
+import processing.core.PApplet;
+
+public class Window extends PApplet {
+  public static void main(String[] args) {
+    PApplet.main("org.bcit.comp2522.project.Window");
+  }
+
+  public void settings() {
+    size(640, 360);
+  }
+
+  public void setup() {
+    background(0);
+  }
+
+  public void draw() {
+    background(0);
+  }
+}
+
+
