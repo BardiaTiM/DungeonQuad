455388b6baad7c289bb84dfe51e93dec770a7800
eanternet <gathrean@icloud.com>
2023-02-24 14:32:26 -0800

    Added boolean alive, void takeDamage(), boolean isAlive() for all three enemies, and added appropriate comments.

--- src/main/java/org/bcit/comp2522/project/Skeleton.java
+++ src/main/java/org/bcit/comp2522/project/Skeleton.java
@@ -5,20 +5,40 @@ package org.bcit.comp2522.project;
  */
 public class Skeleton extends Waves{
 
+  // Skeleton stats
   int arrow_speed;
   int fire_rate;
   double arrow_damage;
 
+  // Skeleton position
   int x_pos;
   int y_pos;
 
+  // Skeleton health
+  boolean alive = true;
+
+  // Moving the skeleton
   public int Move(int x, int y){
     x_pos = x;
     y_pos = y;
     return 0;
   }
 
+  // Skeleton's weaopon of choice
   public void shootArrow(int arrow_speed, int fire_rate, double arrow_damage){
     //throw arrow
+  }
+
+  // Method to see if skeleton is taking damage
+  public void takeDamage(double damage) {
+    // Update health status when skeleton takes damage
+    if (damage >= 1.0) {
+      alive = false;
+    }
+  }
+
+  // Checks to see if skeleton is still Alive
+  public boolean isAlive() {
+    return alive;
   }
 }
--- src/main/java/org/bcit/comp2522/project/Troll.java
+++ src/main/java/org/bcit/comp2522/project/Troll.java
@@ -4,20 +4,41 @@ package org.bcit.comp2522.project;
  * Troll class.
  */
 public class Troll extends Waves{
+
+  // Troll stats
   int rocks_speed;
   int fire_rate;
   double rocks_damage;
 
+  // Troll position
   int x_pos;
   int y_pos;
 
+  // Troll health
+  boolean alive = true;
+
+  // Moving the Troll
   public int Move(int x, int y){
     x_pos = x;
     y_pos = y;
     return 0;
   }
 
+  // Troll's weaopon of choice
   public void throwRock(int rocks_speed, int fire_rate, double rocks_damage){
     //throw rock
+  }
+
+  // Method to see if Troll is taking damage
+  public void takeDamage(double damage) {
+    // Update health status when troll takes damage
+    if (damage >= 1.0) {
+      alive = false;
+    }
+  }
+
+  // Checks to see if Troll is still Alive
+  public boolean isAlive() {
+    return alive;
   }
 }
