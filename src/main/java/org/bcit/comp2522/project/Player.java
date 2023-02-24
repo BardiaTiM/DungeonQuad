126f346d6fd152c1270c1b3990400d343c13340f
Bardia <bilbeigitimouri@my.bcit.ca>
2023-02-24 13:53:03 -0800

    Created Player.java template.

---
+++ src/main/java/org/bcit/comp2522/project/Player.java
@@ -0,0 +1,118 @@
+package org.bcit.comp2522.project;
+
+import java.util.Scanner;
+
+public class Player {
+  protected double exp;
+  protected String playerName;
+  protected int xPos;
+  protected int yPos;
+  protected float speedBoost;
+  protected boolean wings = false;
+  protected boolean isOutOfBounds = false;
+
+  //getters
+  public double getExp() {
+    return exp;
+  }
+
+  public String getPlayerName() {
+    return playerName;
+  }
+
+  public int getXPos() {
+    return xPos;
+  }
+
+  public int getYPos() {
+    return yPos;
+  }
+
+  public float getSpeedBoost() {
+    return speedBoost;
+  }
+
+  public boolean getWings() {
+    return wings;
+  }
+
+  public boolean getIsOutOfBounds() {
+    return isOutOfBounds;
+  }
+
+  public void setExp(double exp) {
+    this.exp = exp;
+  }
+
+  public void setPlayerName(String playerName) {
+    this.playerName = playerName;
+  }
+
+  public void setXPos(int xPos) {
+    this.xPos = xPos;
+  }
+
+  public void setYPos(int yPos) {
+    this.yPos = yPos;
+  }
+
+  public void setSpeedBoost(float speedBoost) {
+    this.speedBoost = speedBoost;
+  }
+
+public void setWings(boolean wings) {
+    this.wings = wings;
+  }
+
+  public void setIsOutOfBounds(boolean isOutOfBounds) {
+    this.isOutOfBounds = isOutOfBounds;
+  }
+
+
+  public Player(String playerName, int xPos, int yPos, float speedBoost, boolean wings, boolean isOutOfBounds) {
+    this.playerName = playerName;
+    this.xPos = xPos;
+    this.yPos = yPos;
+    this.speedBoost = speedBoost;
+    this.wings = wings;
+    this.isOutOfBounds = isOutOfBounds;
+  }
+
+  public void move(char key) {
+    //keyboard keys to move player every direction (WASD)
+    if (key == 'w') {
+      yPos -= 5;
+    } else if (key == 'a') {
+      xPos -= 5;
+    } else if (key == 's') {
+      yPos += 5;
+    } else if (key == 'd') {
+      xPos += 5;
+    }
+  }
+
+  public int takeDamage(int damage) {
+    return damage;
+  }
+
+  public double expGain(double exp) {
+    return exp;
+  }
+
+
+  public static void main(String[] args) {
+    Player player = new Player("Player", 0, 0, 0, false, false);
+    Scanner scanner = new Scanner(System.in);
+    char key = ' ';
+
+    //scan for keyboard input
+    while (key != 'q') {
+      key = scanner.next().charAt(0);
+      player.move(key);
+      System.out.println("Player position: (" + player.getXPos() + ", " + player.getYPos() + ")");
+    }
+
+    player.takeDamage(10);
+    player.expGain(10);
+  }
+}
