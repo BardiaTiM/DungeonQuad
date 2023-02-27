8d06eb7df0e2a9b7ba7ae7654698fc7990f75244
Heeho Ryou <heeho3464@gmail.com>
2023-02-26 18:26:25 -0800

    Axe class created and extends Weapon

---
+++ src/main/java/org/bcit/comp2522/project/Axe.java
@@ -0,0 +1,18 @@
+package org.bcit.comp2522.project;
+
+public class Axe extends Weapon{
+
+
+  public Axe(int upgradeLevel, int bulletsPerMag, int maxMags) {
+    super(upgradeLevel, bulletsPerMag, maxMags);
+  }
+
+  public void fire() {
+
+  }
+
+  @Override
+  public void reload() {
+
+  }
+}
