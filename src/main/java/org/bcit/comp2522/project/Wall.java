f78bc28a0a64e97d68eb5bc29f0ea9508c08d2be
Will Ondrik <willondrik@icloud.com>
2023-03-14 15:00:34 -0700

    Made some changes to the Wall class and how it handles collisions.

--- src/main/java/org/bcit/comp2522/project/Wall.java
+++ src/main/java/org/bcit/comp2522/project/Wall.java
@@ -1,41 +1,35 @@
 package org.bcit.comp2522.project;
 
+import java.awt.*;
+
 public class Wall implements Collidable {
   protected int xPos;
   protected int yPos;
   protected int wallHeight = 500;
   protected int wallWidth = 500;
 
-
-  public Wall (int xPos, int yPos, int wallWidth, int wallHeight){
+  public Wall(int xPos, int yPos, int wallWidth, int wallHeight) {
     this.xPos = xPos;
     this.yPos = yPos;
     this.wallHeight = wallHeight;
     this.wallWidth = wallWidth;
   }
 
-  private Rectangle getGrid(){
+  private Rectangle getGrid() {
     return new Rectangle(xPos, yPos, wallWidth, wallHeight);
   }
 
-
-  /**
-   * This checks if the other sprite collides with the game walls.
-   * @param other
-   * @return true/false
-   */
   @Override
-  public boolean collidesWith(Sprite other){
-
-
+  public boolean collidesWith(Sprite other) {
+    Rectangle wall = getGrid();
+    Rectangle obj = other.getBoundingBox();
+    return wall.intersects(obj);
   }
 
-
   @Override
-  public void handleCollision(Sprite other){
-
+  public void handleCollision(Sprite other) {
+    if (collidesWith(other)) {
+      other.setVelocity(0, 0);
+    }
   }
-
-
-
 }
