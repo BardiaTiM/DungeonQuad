9a1a0c29ffb8df7b15b0da2753bf3850f09fa474
eanternet <gathrean@icloud.com>
2023-03-21 08:55:04 -0700

    Edited dialogue.

--- src/main/java/org/bcit/comp2522/project/Bullet.java
+++ src/main/java/org/bcit/comp2522/project/Bullet.java
@@ -57,28 +57,22 @@ class Bullet extends Collidable {
   void collide() {
     for (int i = 0; i < goblin.length; i++) {
       if (Collidable.collides(x, y, size, goblin[i].x, goblin[i].y, goblin[i].diameter)) {
-        //make goblin stop moving
+        // Make goblin stop moving
         goblin[i].y = 1000;
-
         bullets.remove(this);
-
-        System.out.println("hit");
+        System.out.println("Goblin fainted!");
       }
       if (Collidable.collides(x, y, size, skeleton[i].x, skeleton[i].y, skeleton[i].diameter)) {
-        //make goblin stop moving
+        // Make Skeleton stop moving
         skeleton[i].y = 1000;
-
         bullets.remove(this);
-
-        System.out.println("hit");
+        System.out.println("Skeleton fainted!");
       }
       if (Collidable.collides(x, y, size, troll[i].x, troll[i].y, troll[i].diameter)) {
-        //make goblin stop moving
+        // Make Troll stop moving
         troll[i].y = 1000;
-
         bullets.remove(this);
-
-        System.out.println("hit");
+        System.out.println("Troll fainted");
       }
 
     }
