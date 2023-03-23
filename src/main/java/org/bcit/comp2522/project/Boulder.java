74b2b8c0e31c10bb803a802be725dd417ca06d21
eanternet <gathrean@icloud.com>
2023-03-23 11:30:43 -0700

    Commented out unused class.

--- src/main/java/org/bcit/comp2522/project/Boulder.java
+++ src/main/java/org/bcit/comp2522/project/Boulder.java
@@ -1,30 +1,30 @@
-package org.bcit.comp2522.project;
-
-/**
- * This is the Troll's weapon of choice, the Boulder.
- *
- * @author Gathrean Dela Cruz
- * @version 1.0
- */
-public class Boulder extends Weapon{
-
-      /**
-       * Boulder constructor.
-       *
-       * @param upgradeLevel  upgrade level
-       * @param bulletsPerMag bullets per mag
-       * @param maxMags       max mags
-       */
-      public Boulder(int upgradeLevel, int bulletsPerMag, int maxMags) {
-          super(upgradeLevel, bulletsPerMag, maxMags);
-      }
-
-      public void fire() {
-        //
-      }
-
-      @Override
-      public void reload() {
-        //
-      }
-}
+//package org.bcit.comp2522.project;
+//
+///**
+// * This is the Troll's weapon of choice, the Boulder.
+// *
+// * @author Gathrean Dela Cruz
+// * @version 1.0
+// */
+//public class Boulder extends Weapon{
+//
+//      /**
+//       * Boulder constructor.
+//       *
+//       * @param upgradeLevel  upgrade level
+//       * @param bulletsPerMag bullets per mag
+//       * @param maxMags       max mags
+//       */
+//      public Boulder(int upgradeLevel, int bulletsPerMag, int maxMags) {
+//          super(upgradeLevel, bulletsPerMag, maxMags);
+//      }
+//
+//      public void fire() {
+//        //
+//      }
+//
+//      @Override
+//      public void reload() {
+//        //
+//      }
+//}
