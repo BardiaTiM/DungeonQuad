90158d6d48013bacdb50efbb99a158c74bc65009
Gathrean Dela Cruz <gathrean@icloud.com>
2023-03-25 19:34:36 -0700

    Made setters and getters for Tests.

--- src/main/java/org/bcit/comp2522/project/Arrow.java
+++ src/main/java/org/bcit/comp2522/project/Arrow.java
@@ -6,11 +6,10 @@ public class Arrow {
   float x;
   float y;
   private final float vy;
-  float size = 10;
+  float size;
 
-  private final PImage arrowImage;
   private final Window window;
-  private Skeleton skeleton;
+  private final PImage arrowImage;
 
   public Arrow(float x, float y, float vy, float size, Window window) {
     this.x = x;
@@ -31,6 +30,34 @@ public class Arrow {
 
   public void draw() {
     this.drawArrow(this.x, this.y, 30);
+  }
+
+  public float getX() {
+    return x;
+  }
+
+  public float getY() {
+    return y;
+  }
+
+  public float getSize() {
+    return size;
+  }
+
+  public float getVY() {
+    return vy;
+  }
+
+  public void setX(float x) {
+    this.x = x;
+  }
+
+  public void setY(float y) {
+    this.y = y;
+  }
+
+  public void setSize(float size) {
+    this.size = size;
   }
 
 }
